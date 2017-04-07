f2d45ace14ce552cdf07970941ed67a00eda2560
thegenuinegourav <gourav96suri@gmail.com>
2017-04-07 18:05:45 +0530

    Created and Added app_bar in Module1 Activity

--- app/src/main/java/thegenuinegourav/ignou/activity/Module1Activity.java
+++ app/src/main/java/thegenuinegourav/ignou/activity/Module1Activity.java
@@ -1,9 +1,16 @@
 package thegenuinegourav.ignou.activity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import thegenuinegourav.ignou.R;
+
 
 public class Module1Activity extends AppCompatActivity {
 
@@ -11,5 +18,20 @@ public class Module1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module1);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        mTitle.setText("Module 1");
+        ImageButton back = (ImageButton) toolbar.findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
